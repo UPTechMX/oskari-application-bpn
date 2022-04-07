b478717170c2d2105ec60cf9c0579cddfa0d448c
Urban Planning Technology <63308105+UPTechMX@users.noreply.github.com>
2022-04-07 15:59:04 -0500

    Update en.js

--- bundles/sample/sample-info/resources/locale/en.js
+++ bundles/sample/sample-info/resources/locale/en.js
@@ -4,7 +4,7 @@ Oskari.registerLocalization(
         "key": "sample-info",
         "value": {
             "guidedTour": {
-                "title": "Logging in",
+                "title": "Using the UPT",
                 "message": "To make use of the Urban Planning Tools (UPT), you need to be logged in, as well as belong to the 'uptadmin' and 'uptuser' roles/organizations. Once logged in, click on the 'Tools' menu, then select either UrbanPerformance (UP) or SuitAbility(ST) to start the respective tool!"
             }
         }
