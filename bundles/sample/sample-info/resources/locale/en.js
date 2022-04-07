d05a4f49808a1759302711d543af1e21c87c9494
Urban Planning Technology <63308105+UPTechMX@users.noreply.github.com>
2022-04-07 16:35:30 -0500

    Update en.js

--- bundles/sample/sample-info/resources/locale/en.js
+++ bundles/sample/sample-info/resources/locale/en.js
@@ -5,7 +5,7 @@ Oskari.registerLocalization(
         "value": {
             "guidedTour": {
                 "title": "Using the UPT",
-                "message": "Once logged in, you can click on the "Tools" menu and select Urban Performance (UP) or SuitAbility (ST) to start each tool, respectively."
+                "message": "Once logged in, you can click on the \"Tools\" menu and select Urban Performance (UP) or SuitAbility (ST) to start each tool, respectively."
             }
         }
     });
